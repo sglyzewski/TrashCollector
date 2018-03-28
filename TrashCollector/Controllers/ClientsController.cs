de46e8dec9e3e10e935384cae62ed49750b667e5
Stephanie Glyzewski <stephanie.glyzewski@gmail.com>
2018-03-28 09:19:40 -0500

    built client and worker MVC

---
+++ TrashCollector/Controllers/ClientsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrashCollector.Models;
+using System.Data.Entity;
+
+namespace TrashCollector.Controllers
+{
+    public class ClientsController : Controller
+    {
+        // GET: Clients
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
